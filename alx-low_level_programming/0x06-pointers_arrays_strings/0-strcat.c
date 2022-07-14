df07ad48fc0b6434a36ef19a9258a516a35ab1a2
Seun0001 <oluwaseunafolabi941@gmail.com>
2022-07-14 07:04:11 -0700

    strcat

--- alx-low_level_programming/0x06-pointers_arrays_strings/0-strcat.c
+++ alx-low_level_programming/0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* _strcat - Concatenates two strings
-* @dest: The destination string
-* @src: The source string
-*
-* Return: A pointer to the resulting string dest
-*/
+ * _strcat - a function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: two concat strings
+ */
+
 char *_strcat(char *dest, char *src)
 {
-int dlen = 0, i;
 
-while (dest[dlen])
+int i, n;
+
+for (i = 0; dest[i] != '\0'; i++)
 {
-dlen++;
+
 }
 
-for (i = 0; src[i] != 0; i++)
+for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
 {
-dest[dlen] = src[i];
-dlen++;
-}
 
-dest[dlen] = '\0';
+}
 return (dest);
 }
